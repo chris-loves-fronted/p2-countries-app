fbe9af5dda75b41e2aa7e2a6e41b7a4e737a6da6
chris-loves-frontend <cseyboth@mailbox.org>
2024-05-03 07:27:52 +0200

    Resp. & Col.Mode

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ import { UI } from './UI.js';
 // Global Scope Variables
 const loadingSpinner = document.getElementById('loadingSpinner');
 const searchField = document.getElementById('countrySearch');
+const searchIcon = document.getElementById('searchIcon');
 const selectField = document.getElementById('continentFilter');
 const mainGrid = document.querySelector('main');
+const websiteLogo = document.getElementById('websiteLogo');
+const colorModeSwitcher = document.getElementById('colorModeSwitcher');
+const bodyTag = document.querySelector('body');
 
 let countries = [];
 let continents = [];
@@ -57,6 +61,33 @@ document.addEventListener('CountriesFetched', () => {
 
         mainGrid.innerHTML = '';
         UI.renderCountryCards(filteredCountries, mainGrid);
+    });
+});
+
+// Switch Color Mode
+document.addEventListener('DOMContentLoaded', () => {
+    colorModeSwitcher.addEventListener('click', () => {
+        bodyTag.setAttribute(
+            'data-color-mode',
+            bodyTag.getAttribute('data-color-mode') === 'dark' ? 'light' : 'dark'
+        );
+
+        let colorMode = bodyTag.getAttribute('data-color-mode');
+
+        websiteLogo.setAttribute(
+            'src',
+            colorMode === 'light' ? '/assets/logo-dark.png' : '/assets/logo-white.png'
+        );
+
+        searchIcon.setAttribute(
+            'src',
+            colorMode === 'light' ? '/assets/search-black.svg' : '/assets/search-white.svg'
+        );
+
+        colorModeSwitcher.setAttribute(
+            'src',
+            colorMode === 'light' ? '/assets/moon.svg' : '/assets/sun.svg'
+        );
     });
 });
 
